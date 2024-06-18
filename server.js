c501571536f7d2d02427e652d0e6404fce7dea67
Katherine <katherine.bensley.93@gmail.com>
2024-06-17 21:14:27 -0400

    server can now render /notes

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.render(__dirname + '/public/notes.html');
-    });
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+//  render html
+app.use(express.static('public'));
+
+//  send notes page 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+// const notesRouter = require('./routes/noteroutes');
+// app.use('/note', notesRouter);
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
