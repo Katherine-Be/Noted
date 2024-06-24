0eaa9efdc0511352e55f782f7700d5c1878db6b2
Katherine <katherine.bensley.93@gmail.com>
2024-06-24 19:11:30 -0400

    changed port to a variable

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const fs = require('fs');
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -53,8 +54,8 @@ app.delete("/api/notes/:id", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log('Server is running on ${PORT}')
 });
 
 // const notesRouter = require('./routes/note');
